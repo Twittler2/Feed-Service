9cf98ffce482c4e944be2bb59a8e57a5f4fa47cd
Jiajun Chen <jchen0318@gmail.com>
2017-12-15 19:15:43 -0800

    complete data generation

--- helper/feeds.js
+++ helper/feeds.js
@@ -1,20 +1,31 @@
 const express = require('express');
+
 const router = express.Router();
 const cassandra = require('cassandra-driver');
 
-const client = new cassandra.Client({contactPoints: ['127.0.0.1']});
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'] });
 client.connect(function(err, result) {
   console.log('cassandra connected');
 });
 
-const getFeedByUserId = 'SELECT * FROM feedService.feed WHERE user_id = ?';
+const getFeedByUserId = 'SELECT * FROM feedservice.feed WHERE userid = ? ALLOW FILTERING';
 
-router.get('/:user_Id', function(req, res) {
+router.get('/:user_id', function(req, res) {
   client.execute(getFeedByUserId, [req.params.user_id], function(err, result) {
     if (err) {
       res.status(404).send({msg: err});
     } else {
-
+      result = result.rows[0];
+      let respond = {user_id: result.userid};
+      let tweets = result.tweets;
+      if (tweets.length > 20) {
+        respond.tweets = tweets.slice(res.tweets.length - 20);
+      } else {
+        respond.tweets = tweets.slice();
+      }
+      res.status(200).send(respond);
     }
   });
 });
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Path = require('path');
+const feeds = require('../helper/feeds');
+const newTweets = require('../helper/newTweets');
+const createData = require('../database/index');
 
 const PORT = 3000;
 const app = express();
@@ -8,7 +11,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/feeds/user', feeds);
+app.use('/feeds', feeds);
+app.use('/newtweets', newTweets);
+app.use('/createData', createData);
+
+// data.createData(data.client);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 

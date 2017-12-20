a87d6218ce63582cbfd92502163656cf03071b43
Jiajun Chen <jchen0318@gmail.com>
2017-12-20 10:24:01 -0800

    complete post route to tweets

--- helper/newTweets.js
+++ helper/newTweets.js
@@ -8,13 +8,13 @@ client.connect((err, result) => {
   console.log('cassandra connected');
 });
 
-const editFeedByUserId = 'UPDATE feedservice.feed SET tweets = tweets + [?] WHERE user_id = ?';
+const editFeedByUserId = 'UPDATE feedservice.test SET tweets = tweets + ? WHERE user_id = ?';
 
 router.post('/', (req, res) => {
   const { users, tweet } = req.body;
   const queries = [];
   for (let i = 0; i < users.length; i++) {
-    queries.push({ query: editFeedByUserId, params: [tweet, users[i]] });
+    queries.push({ query: editFeedByUserId, params: [[tweet], users[i]] });
   }
   client.batch(queries, { prepare: true })
     .then(() => {
--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-const apm = require('elastic-apm-node').start({
-  appName: 'feed servie',
-  serverUrl: 'http://localhost:8200',
-});
+require('newrelic');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const Path = require('path');
+
 const feeds = require('../helper/feeds');
 const newTweets = require('../helper/newTweets');
 // const createData = require('../database/index');
@@ -12,16 +11,13 @@ const newTweets = require('../helper/newTweets');
 const PORT = 3000;
 const app = express();
 
+// app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(apm.middleware.express());
 
 app.use('/feeds', feeds);
 app.use('/tweets', newTweets);
 // app.use('/createData', createData);
-
-// data.createData(data.client);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 
